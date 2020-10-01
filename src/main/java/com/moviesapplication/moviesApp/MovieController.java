1e9a8793f74a794c032b46abcf94e51d3cf84a29
AndersonElit <rodriarri@gmail.com>
2020-10-01 06:02:25 -0500

    backend first version

--- src/main/java/com/moviesapplication/moviesApp/MovieController.java
+++ src/main/java/com/moviesapplication/moviesApp/MovieController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MovieController {
 	
@@ -37,9 +40,9 @@ public class MovieController {
 		return movieRepo.allMovies();
 	}
 	
-	@RequestMapping(value="/moviesPerGenre", method = RequestMethod.GET)
-	public List<MovieModel> getMoviesPerGenre() {
-		return movieRepo.moviesPerGenre("accion");
+	@RequestMapping(value="/moviesPerGenre/{genre}", method = RequestMethod.GET)
+	public List<MovieModel> getMoviesPerGenre(@PathVariable String genre) {
+		return movieRepo.moviesPerGenre(genre);
 	}
 
 }
